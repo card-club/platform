54e40d521d94d191186e7e7cfe33807b2d408e01
Charif Mews <charif@westaysafe.com>
2023-06-01 11:25:09 +0200

    Add bearer token

--- src/routes/api/ads/+server.ts
+++ src/routes/api/ads/+server.ts
@@ -1,10 +1,19 @@
 import { error, json } from '@sveltejs/kit';
+import { BEARER_TOKEN } from '$env/static/private';
 
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ request }) {
-    const { publisherId, linkAmount } = await request.json();
-    console.log(publisherId, linkAmount)
-    return json({
-        adViews: 50000
-    });
+    const bearer_token = request.headers
+		.get("Authorization")
+		?.split(" ")[1];
+
+    if (bearer_token === BEARER_TOKEN) {
+        const { publisherId, linkAmount } = await request.json();
+        return json({
+            adViews: 50000
+        });
+    }
+    else {
+        throw error(401, {message: "Unauthorized"});
+    }
 }
