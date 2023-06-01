6d478cf028f95af50744f7014635db86a52ca647
Charif Mews <charif@westaysafe.com>
2023-06-01 16:28:05 +0200

    Deploy enc-values endpoint

---
+++ src/routes/api/enc-values/+server.ts
@@ -0,0 +1,19 @@
+import { json } from '@sveltejs/kit';
+import EthCrypto from 'eth-crypto';
+import { BEARER_TOKEN, PRIVATE_KEY } from '$env/static/private';
+
+// FUJI DON PUBLIC KEY, make this configurable in the future
+const DON_PUBLIC_KEY =
+	'a30264e813edc9927f73e036b7885ee25445b836979cb00ef112bc644bd16de2db866fa74648438b34f52bb196ffa386992e94e0a3dc6913cee52e2e98f1619c';
+
+/** @type {import('./$types').RequestHandler} */
+export async function GET({ request }) {
+	const message = { BEARER_TOKEN: BEARER_TOKEN };
+	const signature = EthCrypto.sign(PRIVATE_KEY, EthCrypto.hash.keccak256(JSON.stringify(message)));
+	const payload = { message, signature };
+	const encrypted = await EthCrypto.encryptWithPublicKey(DON_PUBLIC_KEY, JSON.stringify(payload));
+    
+	return json({
+		'0x0': EthCrypto.cipher.stringify(encrypted)
+	});
+}
