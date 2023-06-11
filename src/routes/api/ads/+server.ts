78294c28ddf0423361157751f5125363d95229d3
Charif Mews <charif@westaysafe.com>
2023-06-12 00:39:46 +0200

    Retry incr approach with existing ad

--- src/routes/api/ads/+server.ts
+++ src/routes/api/ads/+server.ts
@@ -20,6 +20,10 @@ export async function POST({ request }) {
 				adMinutes: 1
 			})
 		}
+		const existingAd: number = await redis.get(`publisher:${publisherId}:ad`) || 0;
+		if (existingAd > 3) {
+			throw error(400, { message: 'Advertisment already exists' });
+		};
 
 		const sxt_access_token = await redis.get('sxt_access_token');
 		let publisher_rate: number = (await redis.get(`publisher:${publisherId}:rate`)) || 0;
@@ -72,7 +76,7 @@ export async function POST({ request }) {
 					break;
 			}
 			// TODO: future set this to advertisement ID instead of hardcoded true
-			await redis.set(`publisher:${publisherId}:ad`, true);
+			await redis.incr(`publisher:${publisherId}:ad`);
 			await redis.expire(
 				`publisher:${publisherId}:ad`,
 				Math.ceil(publisher_rate * linkAmount * 60 * 60 * 24)
@@ -82,7 +86,7 @@ export async function POST({ request }) {
 			});
 		} else {
 			// TODO: future set this to advertisement ID instead of hardcoded true
-			await redis.set(`publisher:${publisherId}:ad`, true);
+			await redis.incr(`publisher:${publisherId}:ad`);
 			await redis.expire(
 				`publisher:${publisherId}:ad`,
 				Math.ceil(publisher_rate * linkAmount * 60 * 60 * 24)
